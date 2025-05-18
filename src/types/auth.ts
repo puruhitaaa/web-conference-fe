a40b4344c35015065e0be4508975b8ba430467b9
Rheno Febrian <rfebrianc@gmail.com>
2025-05-18 18:17:34 +0700

    ganti role jadi string dan parse role ke number saat login serta mengubah auhtorization di frontend

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,7 +1,7 @@
 export interface User {
-  id: number
-  username: string
-  name: string
-  email: string
-  role: string
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  role: number;
 }
